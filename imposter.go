2770516fc4a2f26ff4fd349944a435326bc3a8eb
Kevin Qiu <kevin@freshbooks.com>
2014-04-13 20:06:31 -0400

    Set header upon return

--- imposter.go
+++ imposter.go
@@ -15,16 +15,16 @@ type Error struct {
 }
 
 type Preset struct {
-	Method     string                 `json:"method"`
-	Endpoint   string                 `json:"endpoint"`
-	Body       string                 `json:"body"`
-	Header     map[string]interface{} `json:"header"`
-	StatusCode int                    `json:"status_code"`
+	Method     string            `json:"method"`
+	Endpoint   string            `json:"endpoint"`
+	Body       string            `json:"body"`
+	Header     map[string]string `json:"header"`
+	StatusCode int               `json:"status_code"`
 }
 
 var presets = make(map[string]Preset)
 
-func spec(method string, endpoint string) string {
+func rule(method string, endpoint string) string {
 	return fmt.Sprintf("%s %s", method, endpoint)
 }
 
@@ -46,7 +46,7 @@ func CreatePreset(
 		return http.StatusBadRequest,
 			encoder.Must(enc.Encode(&Error{err.Error()}))
 	}
-	presets[spec(preset.Method, preset.Endpoint)] = *preset
+	presets[rule(preset.Method, preset.Endpoint)] = *preset
 	return http.StatusCreated, encoder.Must(enc.Encode(preset))
 }
 
@@ -55,12 +55,15 @@ func PresetRouter(r martini.Router) {
 	r.Post("/", CreatePreset)
 }
 
-func GetMock(req *http.Request, params martini.Params) (int, string) {
+func GetMock(req *http.Request, writer http.ResponseWriter, params martini.Params) (int, string) {
 	method := req.Method
 	endpoint := "/" + params["_1"]
-	preset, ok := presets[spec(method, endpoint)]
+	preset, ok := presets[rule(method, endpoint)]
 	if !ok {
 		return http.StatusNotFound, "{}"
+	}
+	for key, value := range preset.Header {
+		writer.Header().Set(key, value)
 	}
 	return preset.StatusCode, preset.Body
 }
