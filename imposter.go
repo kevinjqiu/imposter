57a29b7067559c093c32df880d8d91e45fb5afa6
Kevin Qiu <kevin@freshbooks.com>
2014-04-22 00:46:42 -0400

    Separate presets from the main imposter code

--- imposter.go
+++ imposter.go
@@ -14,46 +14,6 @@ type Error struct {
 	Message string `json:"message"`
 }
 
-type PresetRequestMatcher struct {
-	Method   string            `json:"method"`
-	Endpoint string            `json:"endpoint"`
-	Body     string            `json:"body"`
-	Headers  map[string]string `json:"headers"`
-}
-
-func (this *PresetRequestMatcher) Match(req *http.Request) bool {
-	fmt.Printf("%s", req.Header)
-	// match header
-	for key, value := range this.Headers {
-		headerValues, ok := req.Header[key]
-		if !ok {
-			return false
-		}
-		for _, requestValue := range headerValues {
-			if requestValue != value {
-				return false
-			}
-		}
-	}
-	// match body
-	bytes, _ := ioutil.ReadAll(req.Body)
-	if string(bytes) != this.Body {
-		return false
-	}
-	return true
-}
-
-type PresetResponse struct {
-	Body       string            `json:"body"`
-	Headers    map[string]string `json:"headers"`
-	StatusCode int               `json:"status_code"`
-}
-
-type Preset struct {
-	Matcher  PresetRequestMatcher `json:"matcher"`
-	Response PresetResponse       `json:"response"`
-}
-
 type PresetList []Preset
 
 type Presets map[string]PresetList
